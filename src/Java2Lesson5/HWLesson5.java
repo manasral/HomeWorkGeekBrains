58cb50d2e4c5ba98216b4d5791ee5a8a3fcf07df
manas <vladimir.apechkin@gmail.com>
2021-06-09 22:29:29 +0300

    Java 2 lesson 5  Home Work

---
+++ src/Java2Lesson5/HWLesson5.java
@@ -0,0 +1,58 @@
+package Java2Lesson5;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class HWLesson5 {
+    private static final int SIZE = 10_000_000;
+    private static final int HALF_SIZE = SIZE / 2;
+
+    public float[] calculate(float[] arr) {
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + arr[i] / 5) * Math.cos(0.2f + arr[i] / 5) * Math.cos(0.4f + arr[i] / 2));
+        return arr;
+    }
+
+    public void OneThread() {
+        float[] arr = new float[SIZE];
+        for (int i = 0; i < arr.length; i++) arr[i] = 1.0f;
+        long a = System.currentTimeMillis();
+        calculate(arr);
+        System.out.println("Первый поток заканчивается на: " + (System.currentTimeMillis() - a));
+    }
+
+    public void TwoThreads() {
+        float[] arr = new float[SIZE];
+        float[] arr1 = new float[HALF_SIZE];
+        float[] arr2 = new float[HALF_SIZE];
+        for (int i = 0; i < arr.length; i++) arr[i] = 1.0f;
+
+        long a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, arr1, 0, HALF_SIZE);
+        System.arraycopy(arr, HALF_SIZE, arr2, 0, HALF_SIZE);
+
+        new Thread() {
+            public void run() {
+                float[] a1 = calculate(arr1);
+                System.arraycopy(a1, 0, arr1, 0, a1.length);
+            }
+        }.start();
+
+        new Thread() {
+            public void run() {
+                float[] a2 = calculate(arr2);
+                System.arraycopy(a2, 0, arr2, 0, a2.length);
+            }
+        }.start();
+
+        System.arraycopy(arr1, 0, arr, 0, HALF_SIZE);
+        System.arraycopy(arr2, 0, arr, HALF_SIZE, HALF_SIZE);
+        System.out.println("Второй поток заканчивается на: " + (System.currentTimeMillis() - a));
+    }
+
+    public static void main(String s[]) {
+        HWLesson5 o = new HWLesson5();
+        o.OneThread();
+        o.TwoThreads();
+    }
+}
